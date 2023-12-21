9f195eaeee5db179ac1d78b9556b314560e55d4b
Mikaela Hedberg <mie@du.se>
2023-12-21 14:36:48 +0100

    services and full blog functionality

--- server/app.js
+++ server/app.js
@@ -3,11 +3,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use('/posts', require('./routes/postsRoute'));
 app.use('/users', require('./routes/usersRoute'));
 app.use('/tags', require('./routes/tagsRoute'));
