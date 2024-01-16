1a58a2b05be5735e847350d2620f02947cd41373
Mikaela Hedberg <mie@du.se>
2024-01-16 12:58:39 +0100

    Appbar with mui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
 import { Link, Outlet } from 'react-router-dom';
+import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 function App() {
   return (
     <>
-      <ul>
-        <li>
-          <Link to="/">Blogg</Link>
-        </li>
-        <li>
-          <Link to="/posts/new">Skapa inlägg</Link>
-        </li>
-      </ul>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/">Blogg</Link>
+            </Typography>
+            <Button color="inherit">
+              <Link to="/posts/new">Skapa inlägg</Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Outlet />
     </>
   );
