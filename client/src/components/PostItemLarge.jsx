b3463fc3adf64f6e3685992ab20f54824efd8aa6
Mikaela Hedberg <mie@du.se>
2024-01-25 12:00:15 +0100

    postDetail

--- client/src/components/PostItemLarge.jsx
+++ client/src/components/PostItemLarge.jsx
@@ -1,5 +1,23 @@
-function PostItemLarge() {
-  return <h3>PostItemLarge</h3>;
+import Tag from './Tag';
+import UserItemSmall from './UserItemSmall';
+
+function PostItemLarge({ post }) {
+  return (
+    <div style={{ border: '1px solid black' }}>
+      <h3>{post.title}</h3>
+      {post.tags &&
+        post.tags.map((tag) => <Tag key={`tag_${tag}`} text={tag} />)}
+      <div>
+        <p>Skrivet av: </p>
+        <UserItemSmall user={post.author} />
+      </div>
+      <div>
+        <img src={post.imageUrl} height="400" />
+        <p>Skrivet den {post.createdAt}</p>
+        <p>{post.body}</p>
+      </div>
+    </div>
+  );
 }
 
 export default PostItemLarge;
