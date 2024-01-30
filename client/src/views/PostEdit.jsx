405e3cf80f6410233a9d83a49c0c16481d3749e7
Mikaela Hedberg <mie@du.se>
2024-01-30 14:03:02 +0100

    getOne

--- client/src/views/PostEdit.jsx
+++ client/src/views/PostEdit.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getOne } from '../services/PostService';
+
 function PostEdit() {
+  const { id } = useParams();
+
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      getOne(id).then((post) => setPost(post));
+    } else {
+      setPost(null);
+    }
+  }, [id]);
+  console.log(post);
   return <h2>PostEdit</h2>;
 }
 
