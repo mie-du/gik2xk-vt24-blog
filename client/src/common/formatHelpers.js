9b7b285b900810ce528742c988c697235391c8c0
Mikaela Hedberg <mie@du.se>
2024-02-08 16:05:59 +0100

    formathelper

--- client/src/common/formatHelpers.js
+++ client/src/common/formatHelpers.js
@@ -1,6 +1,6 @@
 import { format, formatDistance } from 'date-fns';
 import svLocale from 'date-fns/locale/sv';
-/* L7 Steg 3.3.2 Visa funktioner. toRelativeDateString och truncate */
+
 export function toRelativeDateString(dateString) {
   const date = new Date(dateString);
   return formatDistance(date, new Date(), {
@@ -8,7 +8,7 @@ export function toRelativeDateString(dateString) {
     locale: svLocale
   });
 }
-/* L7 Steg 5.6 Funktion för att formatera. Gör helt som ni vill. Demonstrerar mest date-fns. -> PIL  */
+
 export function toDateTimeString(dateString) {
   const date = new Date(dateString);
   let string = '';
